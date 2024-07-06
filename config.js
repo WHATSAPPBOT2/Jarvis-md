b0b822a51a50ba8305c72ac7ede6ca089dbbf01c
WHATSAPPBOT2 <nikhilsharma2276@gmail.com>
2024-07-06 09:31:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ module.exports = {
   AUDIO_DATA: process.env.AUDIO_DATA || 'ʟᴏᴋɪ-xᴇʀ;ᴊᴀʀᴠɪꜱ;https://i.imgur.com/ep3UbBP.jpg',
   WARN_COUNT: process.env.WARN_COUNT || '3',
   ALIVE_DATA : process.env.ALIVE_DATA || "_*Hy &sender i am alive now*_\n\n_PLATFORM: &platform_\n_RUNTIME : &runtime_\n\n_. type alive to update your alive message_",
-  SESSION_ID: process.env.SESSION_ID || 'Jarvis_0a5f_6a71_c958_035a_c983_c718_202a_d601',
+  SESSION_ID: process.env.SESSION_ID || 'Jarvis_fcaf_46d3_fd54_fe38_a86f_fc43_bc04_26ce',
   LANG: process.env.LANG || 'EN',
   ELEVENLABS: process.env.ELEVENLABS,
   HANDLERS: process.env.HANDLER  || '!',
